9aac648dc431e6a6b08dc4698643eebe586b669e
embeddedLance <hasanijaz.27e@gmail.com>
2019-10-10 18:38:35 +0500

    Create uartRead.ino

---
+++ processing/serial/uartRead.ino
@@ -0,0 +1,20 @@
+// author: Hasan Ijaz
+// description: Open Serial port to read hex values
+void setup()
+{
+  String portName = Serial.list()[0];
+  serial0 = new Serial(this, portName, 9600);
+  println(portName);
+}
+
+void draw()
+{
+
+  if(serial0.available() > 0)
+  {
+    ch = serial0.read();
+    //ch = serial0.readStringUntil('\n');         // read it and store it in val
+    print(ch);
+  }
+  
+}
