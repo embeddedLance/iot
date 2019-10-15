efea932bc0bb42692bd6075d665144e7ada0a7f0
HI <hijaz@innexiv.com>
2019-10-15 16:18:58 +0500

    adding precision to printf

--- embedded-c/data-types/float_precision.c
+++ embedded-c/data-types/float_precision.c
@@ -15,7 +15,7 @@ main(){
               l_d_var = l_d_var/9;
               d_var = l_d_var;
               f_var = l_d_var;
-              printf("%d %f %f %Lf\n", i,
+              printf("%d %.9f %.20f %.25Lf\n", i,
                       f_var, d_var, l_d_var);
               i = i+1;
       }
