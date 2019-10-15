e74baffa0971b45e6359c71b1355cd704e96f571
HI <hijaz@innexiv.com>
2019-10-15 18:10:00 +0500

    adding unsigned comparison with signed example

---
+++ embedded-c/data-types/uint_comp.c
@@ -0,0 +1,17 @@
+//copyright: https://publications.gbdirect.co.uk/c_book/copyright.html
+
+#include <stdio.h>
+#include <stdlib.h>
+main(){
+      int i;
+      unsigned int stop_val;
+
+      stop_val = 0;
+      i = -10;
+
+      while(i <= stop_val){
+              printf("%d\n", i);
+              i = i + 1;
+      }
+      exit(EXIT_SUCCESS);
+}
